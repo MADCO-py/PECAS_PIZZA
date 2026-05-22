d249217ae9615671b8016bc623143c9164c89179
MADCO-py <1782madco@gmail.com>
2026-05-21 20:19:59 -0600

    Initial commit - La Peca en la Pizza

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// vite.config.js
 export default defineConfig({
   plugins: [react()],
-  base: '/PECAS_PIZZA/',   // ← nombre exacto del repo
+  base: '/PECAS_PIZZA/',
 })
